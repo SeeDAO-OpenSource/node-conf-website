ced44b1c09232e81b8de132dc45146561306f163
AAA <7518647+wendychaung@users.noreply.github.com>
2025-02-12 15:13:07 +0800

    fix: update status

--- src/utils/public.ts
+++ src/utils/public.ts
@@ -1,70 +1,68 @@
-import sns from "@seedao/sns-js";
-import CHAIN from "./chain.ts";
-import {USE_NETWORK} from "./constant.ts";
-export function getStatus(type:number) {
-    let str = "";
-    switch (type) {
-        case 0:
-            str="Pending Submit"
-            break;
-        case 1:
-            str="Draft"
-            break;
-        case 2:
-            str="Withdrawn"
-            break;
-        case 3:
-            str="Rejected"
-            break;
-        case 4:
-            str="Approved"
-            break;
-        case 5:
-            str="Voting"
-            break;
-        case 6:
-            str="Vote Passed"
-            break;
-        case 7:
-            str="Vote Failed"
-            break;
-        case 8:
-            str="Pending Execution"
-            break;
-        case 9:
-            str="Executed"
-            break;
-        case 10:
-            str="Execution Failed"
-            break;
-        case 11:
-            str="Vetoed"
-            break;
-        default:
-                break;
-    }
-    return str;
+import sns from '@seedao/sns-js'
+import CHAIN from './chain.ts'
+import { USE_NETWORK } from './constant.ts'
+export function getStatus(type: number) {
+  let str = ''
+  switch (type) {
+    case 0:
+      str = '未提交'
+      break
+    case 1:
+      str = '草稿'
+      break
+    case 2:
+      str = '已撤回'
+      break
+    case 3:
+      str = '已驳回'
+      break
+    case 4:
+      str = '已通过'
+      break
+    case 5:
+      str = '投票中'
+      break
+    case 6:
+      str = '已通过'
+      break
+    case 7:
+      str = '未通过'
+      break
+    case 8:
+      str = '待执行'
+      break
+    case 9:
+      str = '已通过'
+      break
+    case 10:
+      str = '执行失败'
+      break
+    case 11:
+      str = '被否决'
+      break
+    default:
+      break
+  }
+  return str
 }
 
-export const splitWallets = async(wallets:string[]) =>{
-    const chunkSize = 300;
-    const result = [];
-    const resultArr = [];
+export const splitWallets = async (wallets: string[]) => {
+  const chunkSize = 300
+  const result = []
+  const resultArr = []
 
-    for (let i = 0; i < wallets.length; i += chunkSize) {
-        const chunk = wallets.slice(i, i + chunkSize);
-        result.push(chunk);
+  for (let i = 0; i < wallets.length; i += chunkSize) {
+    const chunk = wallets.slice(i, i + chunkSize)
+    result.push(chunk)
+  }
+
+  for await (const chunk of result) {
+    try {
+      const data = await sns.names(chunk, CHAIN[USE_NETWORK].rpcUrls[0])
+      resultArr.push(...data)
+    } catch (error) {
+      console.error(error)
     }
-
-    for await (const chunk of result) {
-        try{
-            const data = await sns.names(chunk, CHAIN[USE_NETWORK].rpcUrls[0]);
-            resultArr.push(...data);
-        }catch(error){
-            console.error(error)
-        }
-
-    }
-    return resultArr;
+  }
+  return resultArr
 }
-

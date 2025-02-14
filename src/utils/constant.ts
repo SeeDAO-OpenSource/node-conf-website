be90e2982e9e9688c81c7e91b8750ad9dd9808a5
AAA <7518647+wendychaung@users.noreply.github.com>
2025-02-14 22:53:07 +0800

    fix: update network

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -2,8 +2,6 @@ export const SELECT_WALLET = 'SEEDAO_WALLET'
 
 const net = import.meta.env.VITE_CHAIN
 
-const net = import.meta.env.VITE_CHAIN
-
 // export const USE_NETWORK: "ETHEREUM" | "POLYGON" = "ETHEREUM";
 
 export const USE_NETWORK: 'ETHEREUM' | 'POLYGON' | 'SEPOLIA' =

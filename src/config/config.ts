f163407feb1d60510061aaa9274c9b492b496307
Frozen <1884084+xrdavies@users.noreply.github.com>
2025-10-26 16:17:38 +0800

    fix: restore endtime

--- src/config/config.ts
+++ src/config/config.ts
@@ -8,7 +8,7 @@ export const CONFERENCE_STAGE: 'adjournment' | 'preparatory' | 'meeting' = 'meet
 export const CURRENT_SEASON = 12 //当前季度
 
 export const CLAIM_START_AT = new Date('2025-10-24T08:00:00+08:00').getTime() // 节点mint开始时间
-export const CLAIM_END_AT = new Date('2025-10-26T20:00:00+08:00').getTime() // 节点mint结束时间
+export const CLAIM_END_AT = new Date('2025-10-26T08:00:00+08:00').getTime() // 节点mint结束时间
 
 // export const CLAIM_START_AT = new Date('2025-02-22T00:00:00+08:00').getTime() // 节点mint开始时间
 // export const CLAIM_END_AT = new Date('2025-02-28T12:00:00+08:00').getTime()
